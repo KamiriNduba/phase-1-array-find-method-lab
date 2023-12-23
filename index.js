43ab06edc09beb500ff6e3c45c9cede840bf6752
NDUBA <user@users-MacBook-Pro.local>
2023-12-23 16:24:18 +0300

    Done'

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+  
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  console.log(superbowlWin(record)); // This will output the year of the win or undefined if not found
+  
